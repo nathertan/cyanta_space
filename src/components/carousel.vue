6cd6a87848334a566ac92c16e84b830f234eff66
nathertan <ndihyan@gmail.com>
2025-08-02 15:55:28 +0700

    udpate: added carousel

---
+++ src/components/carousel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Activities/Projects</h1>
+    <h3>Some "side-quests" i've been doing/involved with.</h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-weight: 500;
+  font-size: 40px;
+  color: white;
+  text-align: center;
+}
+
+h3 {
+  color: white;
+  font-size: 16px;
+  font-weight: 100;
+  text-align: center;
+}
+
+.socials {
+  text-align: center;
+}
+</style>
